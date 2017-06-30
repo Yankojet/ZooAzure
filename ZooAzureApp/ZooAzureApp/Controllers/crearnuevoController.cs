343bc55ff4115ff1dae1c84b873e621e6d791dd5
JoseA <yankojet@gmail.com>
2017-06-30 21:21:22 +0100

    he creado los controller y js

---
+++ ZooAzureApp/ZooAzureApp/Controllers/crearnuevoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ZooAzureApp.Controllers
+{
+    public class crearnuevoController : ApiController
+    {
+        // GET: api/crearnuevo
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/crearnuevo/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/crearnuevo
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/crearnuevo/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/crearnuevo/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ZooAzureApp/ZooAzureApp/Controllers/eliminarController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ZooAzureApp.Controllers
+{
+    public class eliminarController : ApiController
+    {
+        // GET: api/eliminar
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/eliminar/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/eliminar
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/eliminar/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/eliminar/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
